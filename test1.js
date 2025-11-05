753f5457d63cbd47548af35ddfa393a5fa0e5683
md-omar-zahid <rumels@gmail.com>
2025-11-05 18:34:37 +0600

    Added gitignore

--- test1.js
+++
@@ -1,5 +0,0 @@
-const data1 = "data1 variable";
-const data2 = "Data2 variable";
-const data3 = 23234234;
-
-console.log(data1 + "\n " + data2 + " " + data3);
--- typeof.js
+++
@@ -1,9 +0,0 @@
-const data1 = 12324;
-const data2 = "Batch 13";
-const data3 = ["abc", 123, "eerr"];
-const data4 = { name: "MD OMAR ZAHID", designation: "DGM" };
-
-console.log(typeof data3);
-console.log(typeof data1);
-console.log(typeof data2);
-console.log(typeof data4);
